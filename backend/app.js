6059921d7722698c45dc313421168d3db293d3fc
UgniusMin <m.ugnevicius@gmail.com>
2024-07-16 20:14:12 +0300

    antras commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sequelize = require('./config/database');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
 const mechanicRoutes = require('./routes/mechanicRoutes');
@@ -14,6 +14,8 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/mechanics', mechanicRoutes);
+
+mongoose.connect('mongodb+srv://ugnius:ugnius@cluster0.9nuitt7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,9 +4,20 @@ const User = require('../models/user');
 
 exports.register = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, email, password } = req.body;
+    
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User with this email already exists.' });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ username, password: hashedPassword });
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -15,16 +26,23 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ where: { username } });
+    const { email, password } = req.body;
+    
+    if (!email || !password) {
+      return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    const token = jwt.sign({ userId: user.id }, 'secret', { expiresIn: '1h' });
+
+    const token = jwt.sign({ userId: user._id }, 'secret', { expiresIn: '1h' });
     res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ error: error.message });
